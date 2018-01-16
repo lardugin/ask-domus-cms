31c1dd5554ca0d4597142bdc1d528cd8f64d5160
Михаил Лардугин <mihail.lardugin@gmail.com>
2018-01-16 22:09:56 +0300

    404

--- protected/views/error/index.php
+++ protected/views/error/index.php
@@ -4,7 +4,9 @@
 <div class="content-box">
     <section class="container">
         <h1>Ошибка 404</h1>
-
+        <div class="center">
+            <img src="/images/404.jpg" alt="404">
+        </div>
         <p>Вы можете перейти на главную или вернуться на <a href="javascript: history.go(-1);">последнюю просмотренную страницу</a>.</p>
 
         <ul>
